7eb03dac066d286831cfd485dc056d8b400722f0
maquilina27 <94590951+maquilina27@users.noreply.github.com>
2022-05-22 13:58:33 -0700

    Update Water_Sensor.ino

--- Water_Sensor.ino
+++ Water_Sensor.ino
@@ -1,5 +1,9 @@
+#include <SoftwareSerial.h> 
+
 #define maxLiters 0.5
 
+SoftwareSerial MyBlue(6, 5); // RX | TX 
+char flag = '0'; 
 
 int lightPin = 3;
 int sensorPin = 2;
@@ -11,11 +15,12 @@ double totalLiters;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  MyBlue.begin(9600); 
   pinMode(sensorPin, INPUT);
   pinMode(lightPin, OUTPUT);
   digitalWrite(lightPin, LOW);
   attachInterrupt(digitalPinToInterrupt(sensorPin), count, RISING);
-
+  Serial.println("Ready to connect\n"); 
 }
 
 void count(){
@@ -26,10 +31,21 @@ void loop() {
   // put your main code here, to run repeatedly:
   currentTime = millis();
   totalLiters = sensorCount/(double)450;
-  Serial.println("Total Liters: " + String(totalLiters));
-  Serial.println("Sensor Count: " + String(sensorCount));
+  MyBlue.println("Total Liters: " + String(totalLiters));
+  MyBlue.println("Sensor Count: " + String(sensorCount));
   if (totalLiters > maxLiters) {
-      digitalWrite(lightPin, HIGH);  
+      //digitalWrite(lightPin, HIGH);
+      MyBlue.println("Threshold reached\n");
   }
+  //Bluetooth
+  if (MyBlue.available()) 
+   flag = MyBlue.read(); 
+  if (flag == '1') { 
+   MyBlue.println("Reset");
+   sensorCount = 0;
+   totalLiters = 0;
+   flag = '0';
+ }  
+  
   delay(1000);
 }
